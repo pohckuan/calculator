1cbece3fb724c55e98c994b0fe13c788ddb91435
Poh Chuan Kuan <pohckuan@yahoo.com>
2017-05-07 16:14:18 -0400

    backspace & css

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-// let input = document.getElementById('input');
 $('.num').on('click', function(){
   let num = $(this).val();
-  document.calc.result.value += num
+  let value = $('input').val();
+  let input = value += num;
+  $('input').val(input);
 })
 
+// Clear the input box
 $('.clear').on('click', function(){
   $('input').val("");
-
 })
 
-
+// Calculate the input
 function calculate(){
   let ans = eval($('input').val());
-  console.log(typeof(ans))
   $('input').val(ans)
+}
+// 
+// Backspace the last entry
+function backspace(){
+  let inputVal = $('input').val();
+  let deleted = inputVal.substr(0, inputVal.length-1)
+  $('input').val(deleted)
+
 }
