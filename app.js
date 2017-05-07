2864b7c9d0f8b1a236111773bd41ede4d2c91977
Poh Chuan Kuan <pohckuan@yahoo.com>
2017-05-07 14:05:33 -0400

    display ans

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-let input = document.getElementById('input');
-
+// let input = document.getElementById('input');
 $('.num').on('click', function(){
-  let str = []
-  let num = $(this).text();
-  input.textContent += num
-  console.log(num)
-  // let input = document.getElementById('input');
-  // let input += num
+  let num = $(this).val();
+  document.calc.result.value += num
 })
 
 $('.clear').on('click', function(){
-  document.getElementById('input').innerHTML = "";
-  console.log('clear')
+  $('input').val("");
+
 })
+
+
+function calculate(){
+  let ans = eval($('input').val());
+  console.log(typeof(ans))
+  $('input').val(ans)
+}
